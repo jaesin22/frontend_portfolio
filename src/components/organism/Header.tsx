d166351dc5ef206a0e5a46bb379af084df718a8b
Jaesin22 <jaesin22@naver.com>
2024-01-04 20:18:07 +0900

    내일 배포 전 일부 수정     - 페이지 제목 수정     - header 버튼에 a 태크 및 링크 추가

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -9,14 +9,42 @@ const Header = () => {
     <header>
       <div className='w-screen bg-white border-b flex justify-between h-12'>
         <div className='w-3/5 flex justify-between mx-auto'>
-          <Button label='Resume' className={` ${styles}`} renderHoverContent />
+          <a
+            href='https://career.programmers.co.kr/pr/244525_6615'
+            target='_blank'
+            className={` ${styles} flex justify-center items-center`}
+            rel='noreferrer'
+          >
+            <Button label='Resume' renderHoverContent>
+              <span className='sr-only'>text</span>
+            </Button>
+          </a>
+
           <Button
             label='Portfolio'
             className={` ${styles}`}
             renderHoverContent
           />
-          <Button label='Github' className={` ${styles}`} renderHoverContent />
-          <Button label='Blog' className={` ${styles}`} renderHoverContent />
+          <a
+            href='https://github.com/Jaesin22'
+            target='_blank'
+            className={` ${styles} flex justify-center items-center`}
+            rel='noreferrer'
+          >
+            <Button label='Github' renderHoverContent>
+              <span className='sr-only'>text</span>
+            </Button>
+          </a>
+          <a
+            href='https://webdiv-diary.tistory.com/'
+            target='_blank'
+            className={` ${styles} flex justify-center items-center`}
+            rel='noreferrer'
+          >
+            <Button label='Blog' renderHoverContent>
+              <span className='sr-only'>text</span>
+            </Button>
+          </a>
         </div>
       </div>
     </header>
